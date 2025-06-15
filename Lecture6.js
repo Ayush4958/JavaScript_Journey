5d210f657411d0e81d30902035fa2854d02144b0
Ayush Bhandari <bhandariayush935@gmail.com>
2025-06-15 23:42:00 +0530

    classes in js

--- Lecture6.js
+++ Lecture6.js
@@ -1,13 +1,34 @@
 // classes in js
-// Declaration of class
 
-class myclass{
-    constructor(name){
+class bird{
+    constructor(name){ 
         this.name = name
-        console.log(`Welcome to our class , dear ${name}`)
+        console.log("Object is been created...")
+    }
+    eats(){
+        console.log("Function is running for eating")
+    }
+    fly(){
+        console.log("Function is running for flying")
     }
 }
 
 
-let student = new myclass('Dev')
-console.log(student)
+class ostrich extends bird { // this is the way to inherit the one class to another
+    constructor(name){
+        super(name) // without this keyword ostrich class can not work because it fetch the parameters from main class constructor
+        console.log("Object is created and he is a ostrich...")
+    }
+
+    eats(){ // we can overwrite the function 
+        super.eats() // we use this line of code for running the same code from parent class
+        console.log("Function is running for eating (in a special way)")
+    }
+    // we can also use set and get method in the classes
+}
+
+let p = new bird("peacock");
+console.log(p)
+
+let o = new ostrich("Shaturmurgh")
+console.log(o)
